45a335d4aa1e4ad5ff44ee8a5726eb69abfc2970
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-10 16:25:59 -0400

    finished

--- application.rb
+++ application.rb
@@ -1,10 +1,9 @@
 class Application
-
+#I will be run whenever there is a request received
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Silvana."
     resp.finish
   end
 
 end
-
--- config.ru
+++ config.ru
@@ -1,3 +1,6 @@
+
+#We need to actually set up an HTTP server that will receive the HTTP request and send it through the above call method
+
 require_relative "./application.rb"
 
 run Application.new
